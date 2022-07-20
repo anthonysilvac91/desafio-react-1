6b50096ac72c5e985fe98529ed92bab6756f573b
anthonysilvac <anthony.silvac@gmail.com>
2022-07-19 21:23:09 -0400

    desafio 2 menu

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <h1>Kiripaper</h1>
+        <button>Inicio</button>
+        <button>Tienda</button>
+        <button>Blog</button>
+        <button>Descargas</button>
+        <button>Contacto</button>
+    </div>
+  )
+}
+
+export default Navbar
